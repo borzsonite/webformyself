b1331ac68f37cff35b3f4195a910d06f4378f592
Sergey Rozhkov <borzsonite@gmail.com>
2019-11-01 17:13:53 +0300

    lesson 6 time: 03.43

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
@@ -7,15 +8,20 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'src/index.html'
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             }
         ]
     }
