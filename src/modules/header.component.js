d136527c55319a3e93c4be22fc7dde083bdc5fb3
borzsonite <borzsonite@gmail.com>
2019-11-01 06:57:03 +0300

    lesson 4 done

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,1 +1,10 @@
 console.log('Header component!');
+import $ from 'jquery'
+
+$('<h1 />')
+    .text('Hello from jquery')
+    .css({
+        textAlign: 'center',
+        color: 'red'
+    })
+    .appendTo($('header'))
